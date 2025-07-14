885569e5c8ae56800ed236b39cc04a0d38a53771
urvashi <urvashikshatriya9@gmail.com>
2025-07-14 22:51:49 +0530

    Project is done

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,14 +3,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
+  const [count , setCount] = useState(0)
+  const handleOnDecrease = ()=>{
+    if (count > 0) {
+      setCount(count-1)
+    } else {
+      alert("Number can't go on negative side")
+    }
+    
+  }
+
+  const handleOnIncrease = ()=>{
+    if (count>=0) {
+      setCount(count+1)
+    } else {
+      alert("Number can't go on negative side")
+    }
+    
+  }
   return (
     <>
       <h1 className='text-center text-white text-4xl mt-6 mb-6' >Hello & Welcome to counter</h1>
       <div className='text-white text-center'>
-         <button className='bg-red-400 mr-2 p-2 rounded-3xl'>Decrease  − </button>
-         {`Count Number`}
-        <button className='bg-red-400 ml-2 p-2 rounded-3xl'>Increase +</button>
+         <button className='bg-red-400 mr-2 p-2 rounded-3xl' onClick={handleOnDecrease}>Decrease  − </button>
+         {`Count Number is ${count}`}
+        <button className='bg-red-400 ml-2 p-2 rounded-3xl' onClick={handleOnIncrease}>Increase +</button>
         
        
       </div>
