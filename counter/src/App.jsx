43065591e9cc838cee047f853dbd5dea9a84f890
urvashi <urvashikshatriya9@gmail.com>
2025-07-14 22:42:38 +0530

    CSS of the project is done

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,7 +6,14 @@ function App() {
   
   return (
     <>
-      <h1 className='text-center text-white text-4xl mt-6' >Hello</h1>
+      <h1 className='text-center text-white text-4xl mt-6 mb-6' >Hello & Welcome to counter</h1>
+      <div className='text-white text-center'>
+         <button className='bg-red-400 mr-2 p-2 rounded-3xl'>Decrease  − </button>
+         {`Count Number`}
+        <button className='bg-red-400 ml-2 p-2 rounded-3xl'>Increase +</button>
+        
+       
+      </div>
     </>
   )
 }
