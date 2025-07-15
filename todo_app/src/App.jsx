2c5923b3e77fed926f9cacf9f1a7b8fd989b3777
urvashi <urvashikshatriya9@gmail.com>
2025-07-15 13:50:40 +0530

    STEP 3: Connect the Input with State (useState)

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [task, setTask] = useState("")
+  const handleOnClick = (e)=>{
+    console.log(task)
+  }
+ 
   return (
     <>
-      <h1 className='text-center text-4xl'>Welcome to Todo App</h1>
+      <h1 className='text-center text-4xl mt-6 text-white'>Welcome to Todo App</h1>
+      <div className='text-center mt-6'> 
+        <input className='bg-gray-500 rounded-sm p-1' value={task} placeholder='Enter your task' onChange={(e)=>setTask(e.target.value)} />
+        <button className='bg-red-500 rounded-2xl p-1 ml-3 text-white' onClick={handleOnClick}> Add Task</button>
+      </div>
+
     </>
   )
 }
