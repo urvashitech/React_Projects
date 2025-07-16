877bf9b5a728a06c068410829282abc44ef8e7b7
urvashi <urvashikshatriya9@gmail.com>
2025-07-16 17:36:42 +0530

    Connected the Input with State (useState)

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,11 +1,20 @@
-
+import { useState } from 'react'
+import './App.css'
 
 function App() {
   
-
+  const [input,setInput] = useState("")
+  const handleOnClick = ()=>{
+    console.log(input)
+  }
   return (
     <>
-    <h1>Welcome to Weather App </h1>
+    <div className=" text-center text-white">
+      <h1 className="text-4xl mt-12 ">Welcome to Weather App </h1>
+      <input className='bg-gray-500 mt-8 p-1 rounded-sm' value={input} onChange={(e)=>setInput(e.target.value)}  placeholder='Enter City name'/>
+      <button className='bg-red-700 p-1 ml-3 rounded-sm' onClick={handleOnClick}>Search 🔍</button>
+    </div>
+    
       
     </>
   )
